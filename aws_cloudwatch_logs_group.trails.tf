d7a18e67c2e1e32c8044196d97db36e5aa603f71
James Woolfenden <james.woolfenden@gmail.com>
2021-05-19 13:48:28 +0100

    key_id not id

--- aws_cloudwatch_logs_group.trails.tf
+++ aws_cloudwatch_logs_group.trails.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudwatch_log_group" "trails" {
   name              = "cloudtrail"
   retention_in_days = var.retention_in_days
-  kms_key_id        = aws_kms_key.cloudtrail.id
+  kms_key_id        = aws_kms_key.cloudtrail.key_id
 }
 
 
